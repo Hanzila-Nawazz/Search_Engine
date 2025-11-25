823acb62076a1a42872d917207ec865412836e9b
Hanzila <hnawaz.bese24seecs@seecs.edu.pk>
2025-11-26 01:27:43 +0500

    Added the whole working code alongwith the proper comments and all functions and returning a .txt lexicon file

--- lexicon.py
+++ lexicon.py
@@ -33,6 +33,21 @@ def lexicon_generator():
     #Here we have created a new column of tokens which consists of list of filtered and final and clean tokens against each file i.e, against each patent in our dataset.
 
     print("Building a unique lexicon set.................\n")
-    lexicon = set() #Here we have created a single new set for our lexicon since we had vectors of words for each file in our token column in the data frame. This vector of words for each patent can have duplicate words or overall there may be duplication . In order to avoid the duplicate words 
+    lexicon = set() #Here we have created a single new set for our lexicon since we had vectors of words for each file in our token column in the data frame. This vector of words for each patent can have duplicate words or overall there may be duplication . In order to avoid the duplicate words we have used a set data structure which ignores the duplicate words and keep only unique words.
     for token_list_of_each_patent in dataframe['tokens']:
-        lexicon.update(token_list_of_each_patent)
+        lexicon.update(token_list_of_each_patent) #We have used the update function rather than add because we are throwing a list of words against each patent.
+
+    print("Total unique words in lexicon : len{lexicon}\n")
+    #Ok , so we could have done it inside the tokenizer but we preffered to do here because we can later use that function in our frequency check without eliminating our duplicate words which is helpful in page ranking.
+
+    print("Sorting the lexicon list in the alphabetical order for better indexing................\n")
+    sorted_lexicon = sorted(list(lexicon)) #Here we have first converted our lexicon set into a list because sort function can be applied to the list not a set since sets are unordered and a list is ordered data strucure.
+
+    print("Generating a .txt lexicon file with correct indices.....................\n")
+    with open("lexicon.txt", "w", encoding="utf-8") as f: #Opening file in write mode alongwith using with open structure that opens the file safely and is closed automatically in case of program crash and prevents the data corruption. If file does exist it overwrites and if not it simply creates a file. We have done this in order to make sure that in case of new document upload lexicon is regenerated safely and efficiently.
+        for word_id, word in enumerate(sorted_lexicon): #This way it automatically asigns the id to the words and uses a for loop to iterate over multiple words along with their ids and write it in the lexicon.txt file and adding a new line character after every line.
+            f.write(f"{word_id} : {word}\n")
+            
+    print("Lexicon file has been successfully generated.\n")
+
+lexicon_generator()
