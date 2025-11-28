66502d05a7e0ea965d2a2cfabf95bddc1ddbadc8
Hanzila <hnawaz.bese24seecs@seecs.edu.pk>
2025-11-28 21:42:55 +0500

    Removed the data set from the repo but kept a copy locally. Another major change in the code which I made is I used the nltk library for auto cleaning of the text from the data set rather than manual cleaning , added the basic try-catch to prevent the abrupt crash of the program. There was one more major change rather than doing it one go causing the program to crash we divied our datagram into manageable chunks to minimize the ram usage and preventing the crash.

--- lexicon.py
+++ lexicon.py
@@ -1,93 +1,99 @@
 import pandas as pd 
 import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import gc
+import os
 
-WORDS_TO_REMOVE = set(["the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on","are", "as", "with", "his", "they", "at", "be", "this", "have", "from", "or", "one", "had", "by", "word", "but", "not", "what", "all", "were", "we", "when", "your", "can", "said", "there", "use", "an", "each", "which", "she", "do", "how", "their", "if"])
-#So we have created a list of words that are the supporting words and we dont actaully need them in our lexicon dictionary 
+#Importing the defined basic supportive words of english language to not include them in the lexicon using nltk library and also creating an object lemmatizer. Lemmatizer is also a predefined library part of nltk which looks for the adverbs to convert to basic verbs making them root words by using english dictionary. 
+stop_words = set(stopwords.words('english'))  
+lemmatizer = WordNetLemmatizer()
 
-#Added a further improvement to distinguish between the similar terms ending with different suffixes yet having the same meaning in the context
-
-def simplify_word(word):
-    if len(word) < 4:
-        return word
-
-    if word.endswith("ational"): return word[:-7] + "ate"  # relational -> relate
-    if word.endswith("tional"):  return word[:-6] + "tion" # conditional -> condition
-    if word.endswith("ization"): return word[:-7] + "ize"  # optimization -> optimize
-    if word.endswith("bility"):  return word[:-6] + "ble"  # capability -> capable
-
-    if word.endswith("ment"): return word[:-4]  # adjustment -> adjust
-    if word.endswith("ness"): return word[:-4]  # darkness -> dark
-    if word.endswith("able"): return word[:-4]  # adjustable -> adjust
-    if word.endswith("ible"): return word[:-4]  # flexible -> flex
-    if word.endswith("less"): return word[:-4]  # wireless -> wire
-    if word.endswith("ship"): return word[:-4]  # friendship -> friend
-
-    if word.endswith("ing"): return word[:-3]   # running -> run
-    if word.endswith("ion"): return word[:-3]   # connection -> connect
-    if word.endswith("ive"): return word[:-3]   # active -> act
-    if word.endswith("ous"): return word[:-3]   # continuous -> continu
-    if word.endswith("ful"): return word[:-3]   # helpful -> help
-    if word.endswith("ize"): return word[:-3]   # modernize -> modern
-    if word.endswith("ate"): return word[:-3]   # activate -> activ
-    if word.endswith("iti"): return word[:-3]   # activity -> activ
-
-    # We require length > 5 here to be safe (don't turn "real" into "re")
-    if word.endswith("al") and len(word) > 5: return word[:-2]  # electrical -> electric
-    if word.endswith("er") and len(word) > 5: return word[:-2]  # driver -> driv
-    if word.endswith("or") and len(word) > 5: return word[:-2]  # creator -> creat
-    if word.endswith("ly"): return word[:-2]                    # quickly -> quick
-    if word.endswith("ed"): return word[:-2]                    # worked -> work
-
-    if word.endswith("s") and not word.endswith("ss") and len(word) > 3:
-        return word[:-1]
-    return word
-
+#Defining a clean and tokenize function which makes all words lowercase then using regular expressions pattern extracting only a-z alphabets , no punctuation no special symbols no numbers then splitting the words into tokens.After that checking for them in the stop words , removing extra spaces and tokens having all same letters , storing the tokens in a list and returning the list.
 def clean_and_tokenize_text(text):
-    text = text.lower() #We make all the text in the lower case to have a consistent dictionary and no case difference affects our search results and lexcion generation.
-    text = re.sub(r'[^a-z0-9]', ' ' , text) #This line is written and is very cruical.This line uses the regular expressions library and with help of them we are excluding evry charcater other than alphabets and digits and sub function does the task of replacing the charactrs other than alphabets and numbers with the empty strings.
-    text = re.sub(r'\s+', ' ', text).strip() #Here extra spaces other than a single space are remoed to make the tokenization easy and clean.
-    tokens = text.split() #Here text is splitted based upon the spaces and is stored in a list named tokens. But this token still can have additional helping words which we need to remove to further clean our lexicon dictionary.
-    filtered_tokens = [] #Creating a list of filtered tokens
-    for word in tokens: #individually checking tokens in the list and only taking those which are not in the list and have length > 1 so only meaningful words are added in our final lexicon.
-        if((word not in WORDS_TO_REMOVE) and (len(word) > 1) and (not word.isdigit())):
-            processed_word = simplify_word(word)
+    text = text.lower()
+    text = re.sub(r'[^a-z]', ' ' , text)
+    text = re.sub(r'\s+', ' ', text).strip() 
+    tokens = text.split() 
+    filtered_tokens = [] 
+    for token in tokens: 
+        if((token not in stop_words) and (len(token) > 2) and (not token.isdigit()) and (not re.fullmatch(r'(.)\1{2,}' , token))):
+            processed_word = lemmatizer.lemmatize(token)
+            processed_word = lemmatizer.lemmatize(processed_word , pos= 'v')
             filtered_tokens.append(processed_word)
     return filtered_tokens
 
 
 #Main code cotaining the logic for creating the lexicon
 def lexicon_generator():
-    print("\nLoading the data set............\n")
+    print("\nLoading the data set...")
+    #Taking only two columns in the lexicon vocab from the data set since these are the only core columns forming the dictionary of the whole lexicon rest would just help in indexing , ranking and cleaning the results in the later stage.
     columns_to_use = ["title" , "abstract"]
-    #We are using only two columns of our data set in our case of our csv files because these are the two only columns generating the lexicon dictionary of our project. Rest would only be used in foward and inverted indexing or ranking the results in the end.
-    dataframe = pd.read_csv("data/patents_dataset.csv" , usecols=columns_to_use) 
-    #We are using pandas library to crawl through our dataset and create our lexicon dictionary.The reason for using pandas is that there are most of the complex tasks that can be done easily with pandas however if we use simple csv file handling that would break down since our data set is huge and much more complex.
-    dataframe = dataframe.fillna("") #Handling the missing / not avaialable values with empty strings to have a refined result
-    #Since we need both our abstract and title to build the lexicon dictionary for our search engine we would first merge them both into a single string in a new column 'text':
-    print("Merging the two columns..................\n")
-    dataframe['text'] = dataframe['title'].astype(str) + " " + dataframe['abstract'].astype(str)
-    #Now we have combined them both.
+    #Since we have a large data set of approx 0.9 million patents so taking them in one go would significantly increase the ram usage and would cause the program to crash so we are dealing it in chunks of 10,000 files/patents.
 
-    print("Tokenizing and Cleaning the text , removing the junk characters and words , generating a clean lexicon............\n")
-    dataframe['tokens'] = dataframe['text'].apply(clean_and_tokenize_text) 
-    #Here we have created a new column of tokens which consists of list of filtered and final and clean tokens against each file i.e, against each patent in our dataset.
 
-    print("Building a unique lexicon set.................\n")
-    lexicon = set() #Here we have created a single new set for our lexicon since we had vectors of words for each file in our token column in the data frame. This vector of words for each patent can have duplicate words or overall there may be duplication . In order to avoid the duplicate words we have used a set data structure which ignores the duplicate words and keep only unique words.
-    for token_list_of_each_patent in dataframe['tokens']:
-        lexicon.update(token_list_of_each_patent) #We have used the update function rather than add because we are throwing a list of words against each patent.
+    #(the reason for taking that large dataset in our case was that we had a dataset having all the core keywords and definition in our title and abstract only. The description would only increase the lookup time and would not significantly contribute to the lexicon because they are more like numerical having details about the patents ownership , dimensions , numerical and methamatical formulae).
+    CHUNK_SIZE = 10000
 
-    print(f"Total unique words in lexicon : {len(lexicon)}\n")
-    #Ok , so we could have done it inside the tokenizer but we preffered to do here because we can later use that function in our frequency check without eliminating our duplicate words which is helpful in page ranking.
+    #Defining a set for lexicon to store data in it from our ram and after that dumping the data from the ram.
+    lexicon = set()
+    csv_file_path = "data/patents_dataset.csv"
 
-    print("Sorting the lexicon list in the alphabetical order for better indexing................\n")
-    sorted_lexicon = sorted(list(lexicon)) #Here we have first converted our lexicon set into a list because sort function can be applied to the list not a set since sets are unordered and a list is ordered data strucure.
+    #Error handling if the file path casues error in global case.
+    if not os.path.exists(csv_file_path):
+        print(f"Error: File not found at the path : {csv_file_path}")
+        return {}
+    
+    #Here our core logic lies all in it. We have made an iterator that would simply iteratre over the chunks that we have made for our data. In each iteration it would perform the cleaning task and then it would dump the data from the ram and storing it in the map and lexicon .txt file too.
+    chunk_iterator = pd.read_csv(csv_file_path , usecols=columns_to_use , chunksize= CHUNK_SIZE) 
 
-    print("Generating a .txt lexicon file with correct indices.....................\n")
-    with open("lexicon.txt", "w", encoding="utf-8") as f: #Opening file in write mode alongwith using with open structure that opens the file safely and is closed automatically in case of program crash and prevents the data corruption. If file does exist it overwrites and if not it simply creates a file. We have done this in order to make sure that in case of new document upload lexicon is regenerated safely and efficiently.
-        for word_id, word in enumerate(sorted_lexicon): #This way it automatically asigns the id to the words and uses a for loop to iterate over multiple words along with their ids and write it in the lexicon.txt file and adding a new line character after every line.
-            f.write(f"{word_id} : {word}\n")
+    print("Processing the files in chunks..")
+
+    #Maintaining the counter for the progress bar
+    total_processed = 0
+    try:
+        #For loop handling multiple chunks of data to keep the program running.
+        for chunk , chunk_dataframe in enumerate(chunk_iterator):
+            #Handling the empty data spots
+            chunk_dataframe = chunk_dataframe.fillna("") 
+            #Combining both the columns so we can simply perform functions on single strings per patent / file.
+            chunk_dataframe['text'] = chunk_dataframe['title'].astype(str) + " " + chunk_dataframe['abstract'].astype(str)
+            #Applying the cleaning function on each patent set of lexicon and we have many of them.
+            chunk_dataframe['tokens'] = chunk_dataframe['text'].apply(clean_and_tokenize_text) 
+    
+            #Storing the tokens in the vector of each patent 
+            for token_list_of_each_patent in chunk_dataframe['tokens']:
+                lexicon.update(token_list_of_each_patent)
             
-    print("Lexicon file has been successfully generated.\n")
+            #Maintainig the counter
+            total_processed += len(chunk_dataframe)
+            print(f"Processing chunk number :  {chunk+1} (Toal Documents : {total_processed})")
+
+            #Deleting the processed ones from the ram to reduce the ram consumption as much as possible.
+            del chunk_dataframe
+            gc.collect()
+    
+    except KeyboardInterrupt:
+        print("User stopped the execution! Exiting!")
+    except Exception as e:
+        print("An error occured during processing the file!")
+
+
+    print(f"Total unique words in lexicon : {len(lexicon)}")
+    print("Sorting the lexicon list in the alphabetical order for better indexing...")
+    #Sorting the list for better searching algorithm later helping in reduncing the time complexity for the search function.
+    sorted_lexicon = sorted(list(lexicon)) 
+
+    #Dfefining the lexcion dictionary which would map the words against their index nmbers would be helping in developing the forward index and the inverted index.
+    lexicon_dictionary = {}
+    print("Generating and mapping lexicon with correct indices...")
+    #Writing in the .txt file 
+    with open("lexicon.txt", "w", encoding="utf-8") as f: 
+
+        for word_id, word in enumerate(sorted_lexicon):
+            f.write(f"{word_id} : {word}\n")
+            lexicon_dictionary[word] = word_id
+            
+    print("Lexicon file has been successfully generated.")
 
 lexicon_generator()
