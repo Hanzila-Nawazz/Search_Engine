fc1dd9fa1223816e5074648aea7b8d87c972adec
rubai200 <rubait2006@gmail.com>
2025-11-30 18:16:48 +0500

    added forward.py contaning required modules and classes for forward indexing

---
+++ forward.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import gc
+import os
+import traceback
+from collections import Counter
+
+# Reuse your existing clean & tokenize function
+from lexicon import clean_and_tokenize_text
+
+
